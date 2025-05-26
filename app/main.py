4efa417f5e551de9e6d92fac3e9e4947309f9e5b
hschmiedhofer <h.schmiedhofer@gmail.com>
2025-05-26 17:33:10 +0200

    add proper project structure.

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import sys
+from fastapi import FastAPI, APIRouter
+from app.routers import review
+
+# print(sys.executable)
+
+
+app = FastAPI()
+
+app.include_router(review.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Hello Hello Hello !"}
---
+++ app/routers/review.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from app.models.models import Editorial
+
+router = APIRouter(
+    prefix="/review", tags=["review"]
+)  # todo add dependencies and responses
+
+
+fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
+
+@router.get("/")
+async def get_test_response():
+    return fake_items_db
+
+
+@router.post("/")
+async def submit_editorial(editorial: Editorial):
+    return f"submitted: {editorial.name}"
