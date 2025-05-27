1de5dd3625ce0b03cfef4aa4381f129b830aa286
hschmiedhofer <h.schmiedhofer@gmail.com>
2025-05-27 10:53:08 +0200

    add preliminary models for testing gemini structured output.

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,17 @@
+from typing import Literal
 from pydantic import BaseModel
 
 
 class Editorial(BaseModel):
     name: str
     text: str
+
+
+class ErrorDetail(BaseModel):
+    original_error_text: str
+    corrected_text: str
+    error_category: Literal["spelling", "grammar"]
+
+
+class ListOfErrors(BaseModel):
+    errors: list[ErrorDetail]
